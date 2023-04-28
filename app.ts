5da66a74673dd89429bc963559a585eca46c1cb2
jwang0110 <johnw7@uci.edu>
2023-04-28 15:54:18 -0700

    added cors, helmet, and dotenv packages

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
