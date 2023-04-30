d3cd49e911885f6803abfb56aa6f6b336a0a56b6
jwang0110 <johnw7@uci.edu>
2023-04-29 23:49:23 -0700

    generate random gen3 pokemon

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import Pokedex from "pokedex-promise-v2";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
+const P = new Pokedex();
 const app = express();
 
 app.use(cors());
@@ -12,6 +14,19 @@ app.use(helmet());
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
+});
+
+app.get("/random", (_, res) => {
+	(async () => {
+		const pokedexStart = 1;
+		const pokedexEnd = 386;
+		const randomPokemonId =
+			Math.floor(Math.random() * (pokedexEnd - pokedexStart + 1)) +
+			pokedexStart;
+
+		const randomPokemon = await P.getPokemonByName(randomPokemonId);
+		res.send(randomPokemon);
+	})();
 });
 
 export default app;
